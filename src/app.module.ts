9deacd44c60f8c6ce7df5fb7b3a7832222b2a193
Vinícius Alves Borba <viniboal28@gmail.com>
2024-02-19 19:11:31 -0300

    feat: add initial notification entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { PrismaService } from "./prisma.service";
+import { AppController } from "./infra/app.controller";
+import { HttpModule } from "./infra/http.module";
+import { PrismaService } from "./infra/prisma.service";
 
 @Module({
-  imports: [],
+  imports: [HttpModule],
   controllers: [AppController],
   providers: [PrismaService],
 })
