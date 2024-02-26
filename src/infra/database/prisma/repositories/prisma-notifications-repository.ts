35246a8b9977795709269b6aefcbb3d220b3eeda
Vinícius Alves Borba <viniboal28@gmail.com>
2024-02-26 20:56:09 -0300

    feat: implemented repo

--- src/infra/database/prisma/repositories/prisma-notifications-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -6,10 +6,10 @@ import { PrismaNotificationMapper } from "../mappers/prisma-notification-mapper"
 
 @Injectable()
 export class PrismaNotificationsRepository implements NotificationsRepository {
-  constructor(private prismaService: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
   async findById(notificationId: string): Promise<Notification> {
-    const notification = this.prismaService.notification.findFirst({
+    const notification = await this.prisma.notification.findUnique({
       where: {
         id: notificationId,
       },
@@ -19,18 +19,45 @@ export class PrismaNotificationsRepository implements NotificationsRepository {
       return null;
     }
 
-    return;
+    return PrismaNotificationMapper.toDomain(notification);
+  }
+
+  async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+    const notifications = await this.prisma.notification.findMany({
+      where: {
+        recipientId,
+      },
+    });
+
+    return notifications.map(PrismaNotificationMapper.toDomain);
+  }
+
+  async countManyByRecipientId(recipientId: string): Promise<number> {
+    const count = await this.prisma.notification.count({
+      where: {
+        recipientId,
+      },
+    });
+
+    return count;
   }
 
   async create(notification: Notification): Promise<void> {
     const raw = PrismaNotificationMapper.toPrisma(notification);
 
-    await this.prismaService.notification.create({
+    await this.prisma.notification.create({
       data: raw,
     });
   }
 
   async save(notification: Notification): Promise<void> {
-    throw new Error("Method not implemented.");
+    const raw = PrismaNotificationMapper.toPrisma(notification);
+
+    await this.prisma.notification.update({
+      where: {
+        id: raw.id,
+      },
+      data: raw,
+    });
   }
 }
