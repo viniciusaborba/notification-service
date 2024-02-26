883559abd7b6a436a898fc98df2227d8a4f56365
Vinícius Alves Borba <viniboal28@gmail.com>
2024-02-26 20:55:52 -0300

    feat: add toDomain mapper func

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,6 +1,9 @@
+import { Content } from "@app/entities/content";
 import { Notification } from "@app/entities/notification";
+import { Notification as RawNotification } from "@prisma/client";
 
 export class PrismaNotificationMapper {
+  // conversion to persistence layer
   static toPrisma(notification: Notification) {
     return {
       id: notification.id,
@@ -10,5 +13,20 @@ export class PrismaNotificationMapper {
       readAt: notification.readAt,
       createdAt: notification.createdAt,
     };
+  }
+
+  static toDomain(raw: RawNotification): Notification {
+    // conversion to domain layer
+    return new Notification(
+      {
+        category: raw.category,
+        content: new Content(raw.content),
+        recipientId: raw.recipientId,
+        readAt: raw.readAt,
+        canceledAt: raw.canceledAt,
+        createdAt: raw.createdAt,
+      },
+      raw.id,
+    );
   }
 }
