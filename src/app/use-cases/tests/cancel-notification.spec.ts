38867a6156a54ebace4000bbabd8ed725f714995
Vinícius Alves Borba <viniboal28@gmail.com>
2024-02-26 20:52:56 -0300

    feat: add cancel-notification unit test

--- src/app/use-cases/tests/cancel-notification.spec.ts
+++ src/app/use-cases/tests/cancel-notification.spec.ts
@@ -1,19 +1,14 @@
 import { InMemoryNotificationsRepository } from "@test/repositories/in-memory-notifications-repository";
 import { CancelNotification } from "../cancel-notification";
-import { Notification } from "@app/entities/notification";
-import { Content } from "@app/entities/content";
 import { NotificationNotFoundError } from "../errors/notification-not-found";
+import { makeNotification } from "@test/factories/notification-factory";
 
 describe("Cancel notification", () => {
   it("should be able to cancel a notification", async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
 
-    const notification = new Notification({
-      category: "social",
-      content: new Content("test content"),
-      recipientId: "example-recipient-id",
-    });
+    const notification = makeNotification();
 
     await notificationsRepository.create(notification);
 
